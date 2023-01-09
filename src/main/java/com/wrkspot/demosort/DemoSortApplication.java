0cadf03e0e5bba4d8e4a7b9a25a133ff73ae3207
Shruthi guru <shruthi.guru@wrkspot.com>
2023-01-09 13:02:32 +0530

    changes

--- src/main/java/com/wrkspot/demosort/DemoSortApplication.java
+++ src/main/java/com/wrkspot/demosort/DemoSortApplication.java
@@ -4,22 +4,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoSortApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoSortApplication.class, args);
-        String[] str = new String[] {"Can", "I", "block", "your", "calendar", "from", "5:30", "to", "6pm", "today", "to", "review/discuss", "on", "test", "case", "scenarios", "for", "HK", "dashboard", "cards"};
+        List<String> str = Arrays.asList("Can", "I", "block", "your", "calendar", "from", "5:30", "to", "6pm", "today", "to", "review/discuss", "on", "test", "case", "scenarios", "for", "HK", "dashboard", "cards", "bills");
         //String[] names = new String[]{"Can block your calendar from 5:30 to 6pm today to review/discuss on test case scenarios for HK dashboard cards"};
-       //String[] strArr1 = str.split(",");
+       //String[] strArr1 = str.split(" ");
 
         // 1.1 print to console
-        System.out.println("Original String[] Arrays :- \n" + Arrays.toString(str) + "\n");
+        System.out.println("Original String[] Arrays :- \n" + str + "\n");
+        Collections.sort(str, Comparator.comparingInt(String::length).reversed());
 
-        // 3. sorting String[] Arrays in Descending-order
-        Arrays.sort(str, (str1, str2) -> Integer.compare(str2.length(), str1.length()));
 
         // 3.1 print descending-order sorted Strings by its Length
-        System.out.print("\nDescending-order Sorted String[] Arrays by its Length :- \n" + Arrays.toString(str));
-        }
+        System.out.print("\nDescending-order Sorted String List "
+                + "by its Length :- \n" + str);
+
+        //2. sorting String[] Arrays in Ascending-order
+        //Arrays.sort(str, Comparator.comparingInt(String::length));
+
+
+        // 2.1 print ascending-order sorted Strings by its Length
+        //System.out.println("\nAscending-order Sorted String[] Arrays by its Length :- \n" + Arrays.toString() + "\n");
     }
+    }
+    //demo-test push changes made in origin
