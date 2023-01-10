847bd7d91db436286dd41d080fe326e0a1c78e23
Shruthi guru <shruthi.guru@wrkspot.com>
2023-01-10 16:12:48 +0530

    Changed

--- src/main/java/com/wrkspot/demosort/DemoSortApplication.java
+++ src/main/java/com/wrkspot/demosort/DemoSortApplication.java
@@ -12,25 +12,10 @@ import java.util.List;
 public class DemoSortApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoSortApplication.class, args);
-        List<String> str = Arrays.asList("Can", "I", "block", "your", "calendar", "from", "5:30", "to", "6pm", "today", "to", "review/discuss", "on", "test", "case", "scenarios", "for", "HK", "dashboard", "cards", "bills");
-        //String[] names = new String[]{"Can block your calendar from 5:30 to 6pm today to review/discuss on test case scenarios for HK dashboard cards"};
-       //String[] strArr1 = str.split(" ");
-
-        // 1.1 print to console
-        System.out.println("Original String[] Arrays :- \n" + str + "\n");
+        String string = "Can I block your calendar from 5:30 to 6pm today to review/discuss on test case scenarios for HK dashboard cards ";
+        List<String> str =Arrays.asList(string.split("\\u0020"));
         Collections.sort(str, Comparator.comparingInt(String::length).reversed());
-
-
-        // 3.1 print descending-order sorted Strings by its Length
-        System.out.print("\nDescending-order Sorted String List "
-                + "by its Length :- \n" + str);
-
-        //2. sorting String[] Arrays in Ascending-order
-        //Arrays.sort(str, Comparator.comparingInt(String::length));
-
-
-        // 2.1 print ascending-order sorted Strings by its Length
-        //System.out.println("\nAscending-order Sorted String[] Arrays by its Length :- \n" + Arrays.toString() + "\n");
-    }
-    }
+        System.out.print(str);
+}
+}
     //demo-test push changes made in origin
