5c51d9c8e6623507007773553d2858d2f325e1c8
Shruthi guru <shruthi.guru@wrkspot.com>
2023-01-11 12:16:49 +0530

    added pojo

---
+++ src/main/java/com/wrkspot/demosort/Student.java
@@ -0,0 +1,52 @@
+package com.wrkspot.demosort;
+
+public class Student {
+
+        private String registerNum;
+        private String studName;
+
+    public String getRegisterNum() {
+        return registerNum;
+    }
+
+    public void setRegisterNum(String registerNum) {
+        this.registerNum = registerNum;
+    }
+
+    public String getStudName() {
+        return studName;
+    }
+
+    public void setStudName(String studName) {
+        this.studName = studName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Float getMarks() {
+        return marks;
+    }
+
+    public void setMarks(Float marks) {
+        this.marks = marks;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    private Integer age;
+        private Float marks;
+        private String address;
+
+    }
